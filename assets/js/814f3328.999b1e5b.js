8a1f2b98d79d6361ebc759c947c07674133bcf60
MrBisquit <MrBisquit@users.noreply.github.com>
2025-08-19 09:32:13 +0000

    deploy: c1171dc8637da819aa7b345c0fb63a779d29c986

---
+++ assets/js/814f3328.999b1e5b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome to Team Choacury","permalink":"/blog/welcome","unlisted":false,"date":"2025-08-19T09:31:06.000Z"}]}')}}]);
